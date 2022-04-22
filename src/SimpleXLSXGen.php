5309e6a002e719ef161cacd154d5e0045859f772
mrjemson <andrew@jemson.org>
2022-04-22 23:21:40 +1000

    Removed duplicate line

--- src/SimpleXLSXGen.php
+++ src/SimpleXLSXGen.php
@@ -577,7 +577,6 @@ class SimpleXLSXGen {
                                     $this->SI[] = $v;
                                     $cv = count( $this->SI ) - 1;
                                     $this->SI_KEYS[ $skey ] = $cv;
-                                    $this->SI_KEYS[ $skey ] = $cv;
                                 }
                             }
                         }
